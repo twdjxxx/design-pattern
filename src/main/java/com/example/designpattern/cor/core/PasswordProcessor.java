615df96b4b3c49676d48e8707ea2da72166d3619
Deng Jianxin <jianxin.deng@thoughtworks.com>
2023-01-06 22:59:08 +0800

    done PasswordProcessor

--- src/main/java/com/example/designpattern/cor/core/PasswordProcessor.java
+++ src/main/java/com/example/designpattern/cor/core/PasswordProcessor.java
@@ -1,12 +1,26 @@
 package com.example.designpattern.cor.core;
 
 import com.example.designpattern.cor.PurchaseRequestDTO;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordProcessor implements PurchaseValidationProcessor{
+
+    private final Map<String, String> passwordMap = new HashMap<>();
+
+    public PasswordProcessor() {
+        passwordMap.put("JD", "123456");
+    }
+
     @Override
     public void process(PurchaseRequestDTO context) {
+        final String userId = context.getUserId();
+        final String paymentPassword = context.getPaymentPassword();
 
+        if(!passwordMap.get(userId).equals(paymentPassword)) {
+            throw new RuntimeException("Incorrect password");
+        }
     }
 }
