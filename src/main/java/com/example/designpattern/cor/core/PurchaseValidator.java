8f135e829a87f0efd92ce0c9e26997bc36afe38f
Deng Jianxin <jianxin.deng@thoughtworks.com>
2023-01-08 21:47:25 +0800

    add all processors to validator

--- src/main/java/com/example/designpattern/cor/core/PurchaseValidator.java
+++ src/main/java/com/example/designpattern/cor/core/PurchaseValidator.java
@@ -9,8 +9,11 @@ public class PurchaseValidator {
 
     private static List<PurchaseValidationProcessor> PURCHASE_VALIDATION_CHAIN;
 
-    public PurchaseValidator(PasswordProcessor passwordProcessor) {
-        PURCHASE_VALIDATION_CHAIN = List.of(passwordProcessor);
+    public PurchaseValidator(PasswordProcessor passwordProcessor,
+                             UserBalanceProcessor userBalanceProcessor,
+                             ProductInventoryProcessor productInventoryProcessor,
+                             ShippingProcessor shippingProcessor) {
+        PURCHASE_VALIDATION_CHAIN = List.of(passwordProcessor, userBalanceProcessor, productInventoryProcessor, shippingProcessor);
     }
 
     public void validate(PurchaseRequestDTO purchaseRequestDTO) {
